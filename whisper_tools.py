fa0ae0c08efcf19ab8ca3447327bcb29c622411c
davidarthurburns <62669323+davidarthurburns@users.noreply.github.com>
2024-01-16 20:48:10 -0600

    Commonized formatting

--- whisper_tools.py
+++ whisper_tools.py
@@ -4,9 +4,9 @@ from docx import Document
 client = OpenAI()
 
 def transcribe_audio(path):
-	with open(path, 'rb') as audio_file:
+	with open(path, "rb") as audio_file:
 		transcription = client.audio.transcriptions.create(
 			model="whisper-1", 
 			file=audio_file,
-			response_format='text')
+			response_format="text")
 	return transcription
