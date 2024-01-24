250673082d6393a38193e50c6ef67a25c3877fdd
davidarthurburns <62669323+davidarthurburns@users.noreply.github.com>
2024-01-23 21:35:32 -0600

    Define OpenAI assistant goals within chat completions prompt

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ client = OpenAI()
 response = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=[
-				{"role": "system", "content": """You are an advanced novel-writing individual. Please take this small piece of writing and add words before and after to form a complete segment of a story."""},
-                {"role": "user", "content": transcribe_audio("ignore\\vlc-record.mp3")}
+				{"role": "system", "content": """You are an interviewer trying to ascertain whether the user would be a good fit for a job as a data scientist. The expectations of this position include: Leading a cross-functional team in the design and deployment of machine learning models, reporting results and breakthroughs to management and executive groups, and staying abreast of current global trends in machine learning and AI. Minimum requirements for this position include: over five years experience in a data scientist role, a Masters in a related STEM field, and the ability to work in the US. After hearing the user's answers, you ask new questions."""},
+				{"role": "assistant", "content": """Why do you believe you would be a good fit for this position?"""},
+                {"role": "user", "content": transcribe_audio("ignore\\test.mp3")}
                 ],
             temperature=0.6
             )
