708a2ef41470a573a20c8cd6d9714f9381d06b3b
davidarthurburns <62669323+davidarthurburns@users.noreply.github.com>
2024-02-13 20:33:08 -0600

    Add base html file for flask operations

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return "Test"
+job_listing = "Please submit a job listing below."
 
 
-@app.route("/sub")
-def sub():
-    return "Subdirectory"
+@app.route("/", methods=["GET", "POST"])
+def index(job_listing=job_listing):
+    if request.method == "POST":
+        job_listing = request.form.get("listing")
+    return render_template("base.html", listing=job_listing)
